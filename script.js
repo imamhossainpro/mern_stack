ba94542feb491497f86b1b2923aac8310f87d475
Imam Hossain <imamhossainpro@gmail.com>
2023-02-28 16:23:49 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const os = require('os')
+
+console.log(os.homedir())
+console.log(os.arch())
+console.log(os.cpus())
+console.log(os.freemem())
+console.log(os.machine())
+console.log(os.homedir())
